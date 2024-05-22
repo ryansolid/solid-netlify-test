7b01985b7bb522ddb290456957694cd83c8d6a88
Ryan Carniato <ryansolid@gmail.com>
2024-05-22 14:10:03 -0700

    Add cache and purge

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,40 @@
-import { createSignal } from "solid-js";
+import { Suspense, createResource, createSignal } from "solid-js";
+import { purgeCache } from "@netlify/functions";
 import "./app.css";
+import { GET } from "@solidjs/start";
+
+const randomNumber = GET(async function() {
+  "use server";
+  return new Response(Math.random().toString(), {
+    headers: {
+      "Cache-Control": "public, max-age=0, must-revalidate",
+      "Netlify-CDN-Cache-Control": "public, s-max-age=31536000",
+      "Cache-Tag": "random",
+    },
+  });
+})
+
+async function purge() {
+  "use server";
+  await purgeCache({tags: ["random"]});
+  return true;
+}
 
 export default function App() {
   const [count, setCount] = createSignal(0);
+  const [random] = createResource(async () => {
+    const res = await randomNumber();
+    return res.text();
+  });
 
   return (
     <main>
       <h1>Hello world!</h1>
+      <Suspense fallback="Loading...">{random()}</Suspense>
       <button class="increment" onClick={() => setCount(count() + 1)}>
         Clicks: {count()}
       </button>
+      <button class="increment" onClick={() => purge()}>Purge</button>
       <p>
         Visit{" "}
         <a href="https://start.solidjs.com" target="_blank">
